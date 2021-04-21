d764add171b28700799d8b4f3462e48906b1a3dc
yangwonpark <ailey@nate.com>
2021-04-21 13:11:00 +0900

    1-2 JSX 문법

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import Hello from './Hello.js';
+import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  };
   return (
-    <div>
+    <>
       <Hello />
-      <Hello />
-      <Hello />
-    </div>
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
